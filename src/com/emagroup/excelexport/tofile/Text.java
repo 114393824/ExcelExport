b98add0457fe6e2f28bce54d8e582391d230e84c
xiaohu <xiaohu@xiaohudeiMac.local>
2015-05-29 20:43:30 +0800

    fix export text code

--- src/com/emagroup/excelexport/tofile/Text.java
+++ src/com/emagroup/excelexport/tofile/Text.java
@@ -8,8 +8,12 @@ package com.emagroup.excelexport.tofile;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.io.Writer;
 import java.util.Iterator;
 import java.util.Set;
 import java.util.Vector;
@@ -22,31 +26,52 @@ public class Text extends CustomFile {
 		File file = new File(exportPath);
 		Iterator<String> iterator = vec.iterator();
 		
-
-		FileWriter fw = null;
-		BufferedWriter writer = null;
-
+		Writer writer;
 		try {
-			fw = new FileWriter(file);
-			writer = new BufferedWriter(fw);
-			while (iterator.hasNext()) {
-				//writer.write(new String(iterator.next().toString().getBytes(),"utf-8"));
-				writer.write(iterator.next().toString());
-				writer.newLine();// 换行
+			writer = new OutputStreamWriter(new FileOutputStream(file), "UTF-8");
+			while(iterator.hasNext()){
+				writer.write(iterator.next().toString() + "\n");
+				
 			}
 			writer.flush();
+			writer.close();
+			
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} finally {
-			try {
-				writer.close();
-				fw.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
 		}
+
+		
+//		FileWriter fw = null;
+//		BufferedWriter writer = null;
+//
+//		try {
+//			fw = new FileWriter(file);
+//			writer = new BufferedWriter(fw);
+//			while (iterator.hasNext()) {
+//				//writer.write(new String(iterator.next().toString().getBytes(),"utf-8"));
+//				writer.write(iterator.next().toString());
+//				writer.newLine();// 换行
+//			}
+//			writer.flush();
+//		} catch (FileNotFoundException e) {
+//			e.printStackTrace();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		} finally {
+//			try {
+//				writer.close();
+//				fw.close();
+//			} catch (IOException e) {
+//				e.printStackTrace();
+//			}
+//		}
 	}
 
 }
